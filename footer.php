691bd1e002b0f487b3c2bb2e7f66a27a84995ebf
Princess Nicole Dizon <160032742+NicoleDizon11@users.noreply.github.com>
2024-09-03 09:51:52 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,38 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +639163715417 </p>
+         <p> <i class="fas fa-envelope"></i> coffeesupplyco@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> G, CXWR+HCJ, Unit 3, Clean Fuel, Alabang–Zapote Rd, 47, Las Piñas, 1750 Metro Manila </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/coffeesupplycoph?mibextid=ZbWKwL"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://www.instagram.com/coffeesupplycoph?igshid=OGQ5ZDc2ODk2ZA=="> <i class="fab fa-instagram"></i> instagram </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; <?php echo date('Y'); ?>. All <span>right reserved.</span> </p>
+
+</section>
